6c1935c24e12eb56e670f096307d5af74b5d769a
KedraMichal <52571532+KedraMichal@users.noreply.github.com>
2019-10-02 14:58:52 +0200

    Analysis of factors influencing burned liters by cars

--- CarsProject.R
+++ CarsProject.R
@@ -1,47 +1,79 @@
 library(raster)
 library(lmtest)
 library(olsrr)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+
 getwd()
-tabela<- read.csv(file="cars.csv",header=TRUE,sep="," )
+tabela<- read.csv(file="cars.csv", header=TRUE, sep="," )
 
 head(tabela)
 
-
+str(tabela)
 ##### Zamiana na jednostki zwyczajowo u?ywane w Polsce
 
 #Zamiana jednostki wagi na  kg
-tabela$weight<-replace(tabela$weight,TRUE, round(tabela$weight/2.2046,2))
+tabela$weight<- replace(tabela$weight, TRUE, round(tabela$weight/2.2046,2))
 #Aby przeliczy? mpg (mile na galon) na l/100 km, trzeba podzieli? 235 przez wynik spalania
-tabela$mpg<-replace(tabela$mpg,TRUE, round(235/tabela$mpg,2))
+tabela$mpg<- replace(tabela$mpg, TRUE, round(235/tabela$mpg,2))
 # zamianna cal szesciennych na l (cal szescienny = 16,387cm^3)
-tabela$displacement<- replace(tabela$displacement,TRUE,round((tabela$displacement*16.387),2))
-tabela$year<-replace(tabela$year,TRUE, round(235/tabela$mpg,2))
+tabela$displacement<- replace(tabela$displacement, TRUE, round((tabela$displacement*16.387), 2))
+tabela$year<- replace(tabela$year, TRUE, tabela$year+1900)
+#names(tabela)[5]<- "weight (kg)"
+names(tabela)[1]<- "burned liters/100km"
+#names(tabela)[3]<- "displacment in cm^3"
+#names(tabela)[6]<- "acceleration to 97km/h in s"
 
-#names(tabela)[5]<-"weight (kg)"
-names(tabela)[1]<-"burned liters/100km"
-#names(tabela)[3]<-"displacment in cm^3"
-#names(tabela)[6]<-"acceleration to 97km/h in s"
+
+tabela$name <- word(tabela$name, 1)
+
+tabela$name<- gsub("chevy", "chevrolet", tabela$name)
+tabela$name <- gsub('maxda', 'mazda', tabela$name)
+tabela$name <- gsub('chevroelt', 'chevrolet', tabela$name)
+tabela$name <- gsub('mercedes benz', 'mercedes-benz', tabela$name)
+tabela$name <- gsub('toyouta', 'toyota', tabela$name)
+tabela$name <- gsub('vokswagen', 'volkswagen', tabela$name)
+
+(unique(tabela$name))
+tabela$name<- factor(tabela$name)
+levels(tabela$name)<- c(1:33) 
+tabela$name
+
+tabela$name<- as.numeric(tabela$name)
 
 
-########### Dob?r zmiennych
+########### Dobór zmiennych
 #Statysyki opisowe
 summary(tabela)
 
 #Wsp??czynnik zmienno?ci
-cv(tabela$`burned liters/100km`)
-cv(tabela$cylinders)
-cv(tabela$displacement)
-cv(tabela$horsepower)
-cv(tabela$weight)
-cv(tabela$acceleration)
-cv(tabela$year)#Ponizej 10%, zmienna mozna wykluczyc
-cv(tabela$origin)
+sapply(tabela, cv) #Ponizej 10%, zmienna mozna wykluczyc
+
 #Macierz korelacji
-corrMattrix<-cor(tabela[,c(1:8)])
+str(tabela)
+corrMattrix<- cor(tabela[,c(1:9)])
+
+#wizualizacja
+
+#1
+wol<- ggplot(data = tabela, aes(x = `burned liters/100km`, y = acceleration))+
+  geom_point(col = "red") + geom_smooth()
+
+#2
+ggplot(data = tabela, aes(x = `burned liters/100km`, y = weight, color = displacement))+
+  geom_point()
+
+#3
+ggplot(data = tabela, aes(x = `burned liters/100km`, y = horsepower, color = factor(cylinders)))+
+  geom_point(shape = 15)+ facet_grid(.~factor(origin))
+
+
+
 #Model wstepny
-lineralmodel<-lm(tabela$`burned liters/100km` ~  cylinders+displacement+horsepower+weight+acceleration+origin , data=tabela)
-summary(lineralmodel)#  R2-83%
-lineralmodel
+linearmodel<- lm(tabela$`burned liters/100km` ~  cylinders+displacement+horsepower+weight+acceleration+origin+name , data=tabela)
+summary(linearmodel)#  R2-83%
+linearmodel
 
 
 ##############Hellwig (Bez zmiennej year)
@@ -238,30 +270,48 @@ hellwig = function(corrYX = NULL, corrX = NULL) {
   })
 }
 
-R0 = corrMattrix[c(2,3,4,5,6,8),1]
+R0 = corrMattrix[c(2,3,4,5,6,8,9),1]
 R0
-R = corrMattrix[c(2,3,4,5,6,8),c(2,3,4,5,6,8)]
-h<-hellwig(R0,R)
+R = corrMattrix[c(2,3,4,5,6,8,9),c(2,3,4,5,6,8,9)]
+h<- hellwig(R0,R)
+hellwig(R0,R)
 #Wybrane zmienne metoda hellwiga:  horsepower, weight  
 
 
 ################ Metoda krokowa wstecz
-step(lineralmodel, direction = "backward")
+step(linearmodel, direction = "backward")
 #Wynik:cylinders,horsepower,weight,acceleration,origin
 
-
-
+#Bootstrap
+library(bootStepAIC)
+boot.stepAIC(lineralmodel, tabela,B =100 , alpha = 0.05)
 
 #############Diagnostyka modelu
 
 #test ramseya
-resettest(lineralmodel) 
+resettest(linearmodel) 
 
 #test Breusch-Pagana
-bptest(lineralmodel)
+bptest(linearmodel)
 
 #Wsp??liniowo?? zmiennych
-ols_vif_tol(lineralmodel)
+ols_vif_tol(linearmodel)
+
+
+library(bootStepAIC)
+boot.stepAIC(lineralmodel, tabela,B =100 , alpha = 0.05)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
